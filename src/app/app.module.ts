3c69850c1558d2ffd1ca60fe073e32b711b9c0c2
Minh Luan <minhluan1875@gmail.com>
2023-09-24 23:05:02 +0700

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,34 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { ShopComponent } from './shop/shop.component';
+import { AccountComponent } from './account/account.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { CartComponent } from './cart/cart.component';
+import { WishlistComponent } from './wishlist/wishlist.component';
+import { AddressComponent } from './address/address.component';
+import { ViewComponent } from './view/view.component';
+import { ChangepasswordComponent } from './changepassword/changepassword.component';
+import { OderCompleteComponent } from './oder-complete/oder-complete.component';
+import { CheckoutComponent } from './checkout/checkout.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ShopComponent,
+    AccountComponent,
+    LoginComponent,
+    RegisterComponent,
+    CartComponent,
+    WishlistComponent,
+    AddressComponent,
+    ViewComponent,
+    ChangepasswordComponent,
+    OderCompleteComponent,
+    CheckoutComponent
   ],
   imports: [
     BrowserModule,
